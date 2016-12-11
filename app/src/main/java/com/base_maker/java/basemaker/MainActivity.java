1d13feec48078b433ab2487927d9f7bebced7ff4
Anton <a.belichenko@tecsynt.com>
2016-12-11 13:02:51 +0200

    create table

--- app/src/main/java/com/base_maker/java/basemaker/MainActivity.java
+++ app/src/main/java/com/base_maker/java/basemaker/MainActivity.java
@@ -2,12 +2,18 @@ package com.base_maker.java.basemaker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView mEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mEditText = (TextView) findViewById(R.id.main_edit_text);
+        mEditText.setText(DBfile.Profile.CREATE);
     }
 }
---
+++ sqlite_db_maker/src/main/java/com/example/Const.java
@@ -0,0 +1,19 @@
+package com.example;
+
+/**
+ * Created by Admin on 10.12.2016.
+ */
+
+public abstract class Const {
+    public static final String CREATE_TABLE = "CREATE TABLE ";
+    public static final String TEXT = " TEXT";
+    public static final String REAL = " REAL";
+    public static final String COMMA = ", ";
+    public static final String INTEGER = " INTEGER";
+    public static final String PRIMARY_KEY = " PRIMARY KEY ";
+    public static final String AUTOINCREMENT = " AUTOINCREMENT";
+    public static final String DROP_TABLE_IF_EXISTS = "DROP TABLE IF EXISTS ";
+
+    public static final String TABLE_NAME = "TABLE_NAME";
+    public static final String CREATE = "CREATE";
+}
